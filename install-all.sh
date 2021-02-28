1a735d57fc05117c58ab99b0ad38e01f3a93f2d4
omame <me@omame.tech>
2021-02-28 03:56:46 +0100

    Update

--- install-all.sh
+++ install-all.sh
@@ -1,5 +1,24 @@
 #!/bin/bash -e
-for f in m* lib* cy*; do
+
+PACKAGES=(
+    'meuikit'
+    'libcyber-system'
+    'cyber-calculator'
+    'cyber-disk-usage'
+    'cyber-dock'
+    'cyber-fm'
+    'cyber-icon-theme'
+    'cyber-kwin-plugins'
+    'cyber-launcher'
+    'cyber-qt-plugins'
+    'cyber-recorder'
+    'cyber-settings'
+    'cyber-terminal'
+    'cyber-wallpapers'
+    'cyber-workspace'
+)
+
+for f in ${PACKAGES[@]}; do
     cd $f
     echo "==> Building and installing $f..."
     makepkg -si --noconfirm
